00dd9fa55e5667256d7bfce668228f2993529b61
Tiago Bono <tiagobono.2009@sicoobunicoob.com.br>
2017-06-20 18:35:13 -0300

    SQL, RabbitMQ and GO

--- src/main/java/sample/camel/SampleCamelRouter.java
+++ src/main/java/sample/camel/SampleCamelRouter.java
@@ -1,16 +1,22 @@
 package sample.camel;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.rabbitmq.RabbitMQConstants;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SampleCamelRouter extends RouteBuilder {
 
+    private static final String AQMP_PERSISTENT = "2";
+
     @Override
     public void configure() throws Exception {
-        from("timer:hello?period=2000")
-                .transform(constant("teste"))
-                .to("log:hello");
+        from("sql:{{app.sql.calculo-margem}}?dataSource=dataSource&consumer.delay={{app.inbound-delay}}&onConsume={{app.sql.calculo-margem-consume}}")
+                .marshal().json(JsonLibrary.Jackson)
+                .log(body().toString())
+                .setHeader(RabbitMQConstants.DELIVERY_MODE, simple(AQMP_PERSISTENT))
+                .to("rabbitmq:{{spring.rabbitmq.host}}/{{app.amqp.calculo-margem}}?queue={{app.amqp.calculo-margem}}&username={{spring.rabbitmq.username}}&password={{spring.rabbitmq.password}}&autoDelete=false");
     }
 
 }
